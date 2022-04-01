270c062c650919385fb63ae4745dacbdc8f37aea
DIVA-Lab-UTMB <48330773+DIVA-Lab-UTMB@users.noreply.github.com>
2022-04-01 16:18:30 -0500

    Add files via upload
    reupload package

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // CoClust
 List CoClust(int nr, int nc, NumericVector data, int ITER);
 RcppExport SEXP _BipartiteModularityMaximization_CoClust(SEXP nrSEXP, SEXP ncSEXP, SEXP dataSEXP, SEXP ITERSEXP) {
